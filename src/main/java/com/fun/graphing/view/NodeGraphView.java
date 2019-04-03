4c424957fdea33ad093cc210bc5832c146320d19
UniHex <32622066+unihex@users.noreply.github.com>
2019-04-03 17:23:08 -0400

    The basic view for the application

--- src/main/java/com/fun/graphing/view/NodeGraphView.java
+++ src/main/java/com/fun/graphing/view/NodeGraphView.java
@@ -2,16 +2,67 @@ package com.fun.graphing.view;
 
 import com.fun.graphing.controller.NodeController;
 
+import javafx.beans.property.ReadOnlyDoubleProperty;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 public class NodeGraphView {
 	private NodeController nodeController;
+	
+	private final static Color BACKGROUND_COLOR = Color.web("#f4aa42");
 	
 	public NodeGraphView(NodeController nodeController) {
 		this.nodeController = nodeController;
 	}
 	
 	public Scene buildView() {
-		return null;
+		VBox vBoxLayout = new VBox();
+		
+		Pane drawingPane = this.buildDrawingPane(vBoxLayout.heightProperty(), vBoxLayout.widthProperty());
+		HBox buttonBox = this.buildButtonBox();
+		
+		vBoxLayout.getChildren().addAll(drawingPane, buttonBox);
+		Scene scene = new Scene(vBoxLayout, 900, 900);
+		return scene;
+	}
+	
+	
+	private Pane buildDrawingPane(ReadOnlyDoubleProperty height, ReadOnlyDoubleProperty width) {
+		Pane pane = new Pane();
+		
+		pane.prefHeightProperty().bind(height);
+		pane.prefWidthProperty().bind(width);
+		
+		BackgroundFill bgFill = new BackgroundFill(BACKGROUND_COLOR, null, null);
+		Background background = new Background(bgFill);
+		
+		pane.setBackground(background);
+		
+		return pane;
+	}
+	
+	private HBox buildButtonBox() {
+		HBox hBox = new HBox();
+		hBox.setAlignment(Pos.BOTTOM_CENTER);
+		hBox.setSpacing(100);
+		
+		Button createNodesButton = new Button("Create Nodes");
+		
+		Button deleteNodesButton = new Button("Delete Nodes");
+		
+		Button connectNodesButton = new Button("Connect Nodes");
+		
+		Button traverseNodesButton = new Button("Traverse Nodes");
+		
+		hBox.getChildren().addAll(createNodesButton, deleteNodesButton, connectNodesButton, traverseNodesButton);
+		
+		return hBox;
 	}
 }
